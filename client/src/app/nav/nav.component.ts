751a719f09504c845f8ed979f3080047e8e74a48
Werner Gomindes <wgomindes@synetec.co.uk>
2024-07-24 11:46:56 +0100

    auth guard and routing and bootswatch additions

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { User } from '../_models/user';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -11,20 +13,23 @@ import { Observable } from 'rxjs';
 export class NavComponent implements OnInit {
   model: any = {}
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) {
+  }
 
   ngOnInit(): void {
   }
 
   login() {
     this.accountService.login(this.model).subscribe(res => {
-      console.log(res);
+      this.router.navigateByUrl('/members');
     }, err => {
-      console.log(err)
+      console.log(err);
+      this.toastr.error(err.error);
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
